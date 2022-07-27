5cffa4c5d77576bd52cc049b8dcd5b0b68b82a14
Deyvid Kasteh <99744584+Deyvid-Kasteh@users.noreply.github.com>
2022-07-27 19:57:14 -0300

    dashboard 10 add animation

--- dashboard/scripts.js
+++ dashboard/scripts.js
@@ -2,7 +2,10 @@ function abrir(a) {
     var oper = a
 
     var menuToggle = document.querySelector('#menuToggle');
+    var central = document.querySelector('#central');
+    var cozinha = document.querySelector('#cozinha');
     var nomeCozinha = document.querySelector('#nomeCozinha');
+    var svg = document.querySelector('#svgCozinha');
     var quarto = document.querySelector('#quarto');
     var sala = document.querySelector('#sala');
     var banheiro = document.querySelector('#banheiro');
@@ -18,14 +21,53 @@ function abrir(a) {
     var energiaExaust =document.querySelector('#bttnExaust');
 
 
+    function espera3s(){
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                console.log('esperando a animação acontecer');
+                resolve()
+            }, 3000);
+        })
+    }
+    function espera1s(){
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                console.log('esperando a animação acontecer');
+                resolve()
+            }, 1000);
+        })
+    }
+
+    async function primeiroPasso() {
+        await espera3s();
+        nomeCozinha.classList.toggle('activeCozinha');
+        nomeCozinha.classList.toggle('animedCozinha');
+        svg.classList.toggle('animedsvgCozinha');
+        await espera3s();
+        central.classList.toggle('activeCentral');
+        svg.classList.toggle('animedsvgCozinha');
+        await espera1s();
+        quarto.classList.toggle('activeQuarto');
+        await espera1s();
+        sala.classList.toggle('activeSala');
+        await espera1s();
+        banheiro.classList.toggle('activeBanheiro');
+        await espera1s();
+        lavanderia.classList.toggle('activeLavanderia');
+    }
+
+
+
     switch (oper) {
         case 'menu':
             menuToggle.classList.toggle('activeMenu');
-            nomeCozinha.classList.toggle('activeCozinha');
-            quarto.classList.toggle('activeQuarto');
-            sala.classList.toggle('activeSala');
-            banheiro.classList.toggle('activeBanheiro');
-            lavanderia.classList.toggle('activeLavanderia');
+            cozinha.classList.toggle('activeCozinhaCentral');
+            nomeCozinha.classList.toggle('animedCozinha');
+            primeiroPasso();
+            //quarto.classList.toggle('activeQuarto');
+            //sala.classList.toggle('activeSala');
+            //banheiro.classList.toggle('activeBanheiro');
+            //lavanderia.classList.toggle('activeLavanderia');
 
 
 
